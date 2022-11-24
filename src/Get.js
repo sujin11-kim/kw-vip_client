21f473d0a1e4f3edffac8228b3a3b47e918b0ae3
limsomi <97082128+limsomi@users.noreply.github.com>
2022-11-24 14:37:23 +0900

    Add files via upload

---
+++ src/Get.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import axios from 'axios';
+
+// function Get (){
+//     axios.get(
+//         ``,
+//         {
+//             header:
+//         }
+//     )
+//     .then(response=>{
+//         console.log(response)
+//         if(response){
+//             console.log(response.data)
+            
+
+//         }
+//     })
+// }
+
+// export default Get;
