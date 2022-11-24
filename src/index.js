9fbb53959577b3984dde02ccb4f1af9949a4dbbf
limsomi <97082128+limsomi@users.noreply.github.com>
2022-11-24 14:39:48 +0900

    Update index.js
    component 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Map from './map';
+import Area_CheckBox from './Component/CheckBox/Area_CheckBox.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Map />
+    <Map></Map>
+    <Area_CheckBox></Area_CheckBox>
   </React.StrictMode>
 );
