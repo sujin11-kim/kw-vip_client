8aa193cccf8e8cef95ab69ca5d43e10015fa00e4
limsomi <97082128+limsomi@users.noreply.github.com>
2022-11-29 23:19:58 +0900

    Delete src/Component/Business directory

--- src/Component/Business/Business.js
+++
@@ -1,8 +0,0 @@
-// import React, {useState,useEffect} from "react";
-// const Business=()=>{
-//     return(
-//         <>
-//         <button id="button_1"></button>
-//         </>
-//     )
-// }
